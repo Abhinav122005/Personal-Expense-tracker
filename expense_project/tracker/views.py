ad37ea70ddb6bca2c6f9bf553e40188d118989ec
Abhinav Rayabarapu <rayabarapuabhinav@gmail.com>
2026-08-05 17:35:17 +0530

    initial commit

--- expense_project/tracker/views.py
+++ expense_project/tracker/views.py
@@ -14,7 +14,7 @@ from django.db.models import Sum
 from django.utils import timezone
 from django.core.paginator import Paginator
 import openpyxl
-import resend
+# import resend
 import json
 
 from django import forms
